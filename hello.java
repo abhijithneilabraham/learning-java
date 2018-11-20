04a2a9eab7fec9dd381282c5918f6e2c278d6a6b
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-20 09:02:19 +0530

    Update hello.java

---
+++ hello.java
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 09:00:40 2018
+
+@author: abhijithneilabraham
+"""
+
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
