eb40676dc024a79fdeae5bfa3183dcf34ce834b4
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-20 14:14:59 +0530

    Hello world

--- hello.java
+++ hello.java
@@ -1,12 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 20 09:00:40 2018
 
-@author: abhijithneilabraham
-"""
 
-public class MyFirstJavaProgram {
+public class Hello {
 
    /* This is my first java program.
     * This will print 'Hello World' as the output
