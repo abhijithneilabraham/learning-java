8716a55d0c3eee9e99b8d86991ad6865bcb4ad10
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-22 01:30:45 +0530

    Polymorphism

---
+++ Bluej/Demo.java
@@ -0,0 +1,16 @@
+class Jack{
+    void say(){
+        System.out.println("All work and no play makes jack a dull boy");
+    }
+}
+public class Demo extends Jack{
+    void say()
+   {
+       System.out.println("Jack and Jill went up the hill");
+    }
+public static void main(){
+    Jack obj=new Demo();
+    obj.say();
+    obj=new Jack();
+    obj.say();
+}}
