1c0e13bc588db2dc9800926df418254bb5fc8a8a
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2018-11-24 22:53:11 +0530

    abstraction

---
+++ Bluej/Bike.java
@@ -0,0 +1,14 @@
+abstract class Bike{
+    abstract void run();
+}
+class Ducatti extends Bike{
+    void run(){
+        System.out.println("fast as lightening");}
+        public static void main(String args[]){
+            Bike obj=new Ducatti();
+            obj.run();
+        }}
+            
+ 
+        
+    
